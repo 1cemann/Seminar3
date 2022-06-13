1ba393c3422df31ff808fd97f8a8c152920b4d3a
Mad <ned_91@mail.ru>
2022-06-13 22:01:05 +0300

    add Test For Construction

---
+++ TestForConstruction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Wwedite N: ");
+int N=int.Parse(ReadLine());
+
+for(int i=0;i<=N;i+=2)
+{
+    WriteLine(i);
+}
+
+//Переменная живёт внутри скобок
