b0eec52879a87a0e6ace8b1a8ddfc820f39143fa
Mad <ned_91@mail.ru>
2022-06-13 21:48:07 +0300

    add task21

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -3,11 +3,10 @@ using static System.Console;
 
 Clear();
 
-
 Write("wwedite x ");
 int x = int.Parse(ReadLine());
 Write("wwedite y ");
-int y = int.Parse(ReadLine());
+int y = int.Parse(ReadLine));
 int quater = 0;
 
 if (x > 0 && y > 0)
---
+++ Task21/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+
+//2 -> 1,4
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("wwedite chislo N: ");
+int N = Convert.ToInt32(ReadLine());
+int count = 1;
+
+while(count<=N)
+{
+    Write($"{Math.Pow(count,2)}, " );
+    count++;
+}
+Write($"{Math.Pow(count,2)}." );
+
