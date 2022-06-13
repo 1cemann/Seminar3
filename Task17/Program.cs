027723a64f28e5bef426a93df8b64aa6911c46c2
Mad <ned_91@mail.ru>
2022-06-13 20:29:00 +0300

    add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+
+Write("wwedite x ");
+int x=int.Parse(ReadLine());
+Write("wwedite y ");
+int y=int.Parse(ReadLine());
+int quater=0;
+
+if(x>0&&y>0)
+{
+    quater=1;
+}
+if(x<0&&y>0)
+{
+    quater=2;
+}
+if(x<0&&y<0)
+{
+    quater=3;
+}
+if(x>0&&y<0)
+{
+    quater=4;
+}
+WriteLine($"{quater} chetwert");
