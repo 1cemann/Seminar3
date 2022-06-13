a459bdb1b0da2bd3078e2fa448a9ae65b5199812
Mad <ned_91@mail.ru>
2022-06-13 20:38:52 +0300

    add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+//*Задача 18:**
+
+//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Write("Input quater ");
+int quater = int.Parse(ReadLine());
+
+if (quater==1) WriteLine("x>0,y>0");
+if (quater==2) WriteLine("x<=0,y>0");
+if (quater==3) WriteLine("x<0,y<0");
+if (quater==4) WriteLine("x>0,y<=0");
